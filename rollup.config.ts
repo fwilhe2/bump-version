f8cf3b4d639643e59fb3a6f79998f5bca070a674
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2026-02-06 21:56:56 +0100

    Update template (#746)
    Update to latest state of actions/typescript-action
    Recent dependency upgrade require ESM, the existing setup was based on CJS
    
    bump actions/toolkit dependencies
    
    Revert to using npm instead of yarn, it is not worth the effort
    
    Drop devcontainer, it was not maintained anyway

---
+++ rollup.config.ts
@@ -0,0 +1,27 @@
+// rollup.config.ts
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
+const config = {
+  input: 'src/main.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: true,
+    inlineDynamicImports: true
+  },
+  // Ensure 'external' is empty or doesn't include action dependencies
+  external: [],
+  plugins: [
+    typescript(),
+    nodeResolve({
+      preferBuiltins: true,
+      extensions: ['.ts', '.js'], // Tells Rollup to look for these extensions
+      exportConditions: ['node'] // Helps resolve node-specific ESM packages
+    }),
+    commonjs()
+  ]
+}
+
+export default config
